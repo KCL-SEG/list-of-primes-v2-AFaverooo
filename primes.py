fe50b9b83f8c097b13bd9c1940a6994be34b64ca
Augusto Favero <augusto.favero@kcl.ac.uk>
2022-10-07 18:12:49 +0100

    Extended primes to check against 0 or negative numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+def is_a_Prime(num):
+    if (num==0) or (num==1):
+        return False
+
+    for i in range(2, num):
+        if (num % i == 0):
+            return False
+    return True
 
 def primes(number_of_primes):
+    if(number_of_primes <= 0):
+        raise ValueError("The number of primes muss be bigger then 0")
+        return
+
     list = []
+    count = 0
+
+    while(len(list) < number_of_primes):
+        if is_a_Prime(count):
+            list.append(count)
+
+        count += 1
     return list
